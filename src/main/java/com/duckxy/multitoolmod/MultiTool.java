9c41593a6146b80852dfe8200bb78b1e39b4b079
sahar <mr.saharat.u@gmail.com>
2026-01-17 03:39:14 +0700

    initial Multi-tool mod

---
+++ src/main/java/com/duckxy/multitoolmod/MultiTool.java
@@ -0,0 +1,16 @@
+package com.duckxy.multitoolmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MultiTool implements ModInitializer {
+	public static final String MOD_ID = "multitool";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/duckxy/multitoolmod/MultiToolModClient.java
@@ -0,0 +1,10 @@
+package com.duckxy.multitoolmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MultiToolModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
